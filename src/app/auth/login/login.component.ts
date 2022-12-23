1bfe0822436bafa3d229cadc814cc64a1fb9c655
sara soliman <38067419+Ssoliman2018@users.noreply.github.com>
2022-12-24 00:44:32 +0200

    setup login page
    setup login page and functionality

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { LoginModel } from '../login-model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.sass']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
 
-  constructor() { }
-
+  ) {
+    this.loginForm = this.formBuilder.nonNullable.group({
+      email: ['kminchelle@qq.com', [Validators.required, Validators.email]],
+      password: ['0lelplR', Validators.required],
+      username: ['kminchelle', Validators.required]
+    })
+  }
   ngOnInit(): void {
+
+  }
+
+  signIn() {
+    const loginModel: LoginModel = {
+      username: this.loginForm.getRawValue().username,
+      password: this.loginForm.getRawValue().password
+    }
+    this.authService.userLogin(loginModel).subscribe((result) => {
+      if (result) {
+        this.router.navigate(['/']);
+      }
+    })
   }
 
 }
