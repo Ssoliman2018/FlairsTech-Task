461200fba0e8154a0b4d05fed9abd79c4520e9e6
sara soliman <38067419+Ssoliman2018@users.noreply.github.com>
2022-12-23 21:31:05 +0200

    created login page
    created login page and configuration of tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthService } from './auth/auth.service';
+import { JWT_OPTIONS, JwtModule } from '@auth0/angular-jwt';
+import { jwtOptionFactor } from './shared/auth/jwtOptionFactor';
+ 
 
+ 
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,6 +18,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    JwtModule.forRoot({
+      jwtOptionsProvider:{
+        provide:JWT_OPTIONS,
+        useFactory: jwtOptionFactor,
+        deps:[AuthService]
+      } 
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
