edc84826220c81965da1590f1b45edc8c5e65091
sara soliman <38067419+Ssoliman2018@users.noreply.github.com>
2022-12-24 21:50:27 +0200

    create user list component
    create user list component
    create form component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { Observable } from "rxjs";
 import { AuthService } from "./auth/auth.service";
 import { UserProfile } from "./shared/auth/user-profile";
+import { Token } from "@angular/compiler";
 
 @Component({
   selector: "app-root",
--- src/app/product/list/list.component.ts
+++ src/app/product/list/list.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/shared/product.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.sass']
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, AfterViewInit {
+  productList: Product[] = [];
+  displayedColumns: string[] = ['id', 'image', 'title', 'price', 'actions'];
+  dataSource!: MatTableDataSource<Product>;
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
-  ngOnInit(): void {
+  constructor(private productService: ProductService) { }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  
+  ngOnInit(): void {
+    this.loadProducts();
+    console.log('product list >>', this.productList)
+  }
+     // Issues list
+     loadProducts() {
+      return this.productService.getProducts().subscribe((data) => {
+        console.log('data', data)
+        this.dataSource = new MatTableDataSource<Product>(data);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort
+      })
+    }
 
 }
