11f793c3c796f9d6ae940a943c9bddaa5e1c3e9f
sara soliman <38067419+Ssoliman2018@users.noreply.github.com>
2022-12-24 12:49:02 +0200

    create side nav menu
    create side nav menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Observable } from "rxjs";
+import { AuthService } from "./auth/auth.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.sass"],
 })
 export class AppComponent {
-  title = 'FlairsTech';
+  title = "FlairsTech";
+  isLoggedIn$: Observable<boolean>;
+  sideBarOpen = true;
+
+  constructor(private authService: AuthService) {
+    this.isLoggedIn$ = this.authService.isLoggedIn;
+  }
+
+
+  sideBarToggler() {
+    this.sideBarOpen = !this.sideBarOpen;
+  }
 }
---
+++ src/app/side-nav/side-nav.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
+
+@Component({
+  selector: 'app-side-nav',
+  templateUrl: './side-nav.component.html',
+  styleUrls: ['./side-nav.component.sass']
+})
+export class SideNavComponent implements OnInit {
+  constructor() { 
+  }
+
+  ngOnInit(): void {
+  }
+
+}
